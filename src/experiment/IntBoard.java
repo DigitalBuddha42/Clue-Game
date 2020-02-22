53b3ee7c81d056c5500db631d60b77f54589c502
Sam Mills <smills1@mymail.mines.edu>
2020-02-21 20:25:34 -0700

    Added comments

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,17 +1,21 @@
-
-
 package experiment;
 
 import java.util.*;
 
+/**
+ * @author Sam Mills, Nadia Bixenman
+ *
+ */
 public class IntBoard {
 	private BoardCell[][] grid;
 	private Map<BoardCell, HashSet<BoardCell>> adjMtx;
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
 	
-	private static final int BOARD_SIZE = 4;
+	private static final int BOARD_SIZE = 4; // Constant representing both width and height of Board
 	
+	/** Constructor for IntBoard - Creates a grid of desired size, initializes the adjacency list, visited list, and target list, and calls calcAdjacencies
+	 */
 	public IntBoard() {
 		grid = new BoardCell[BOARD_SIZE][BOARD_SIZE];
 		for (int i = 0; i < BOARD_SIZE; i++) {
@@ -27,7 +31,10 @@ public class IntBoard {
 		calcAdjacencies();
 	}
 	
+	/** Calculates the adjacency list
+	 */
 	private void calcAdjacencies() {
+		// For each cell in the board
 		for (int i = 0; i < BOARD_SIZE; i++) {
 			for (int j = 0; j < BOARD_SIZE; j++) {
 				HashSet<BoardCell> tempAdj = new HashSet<BoardCell>();
@@ -43,37 +50,55 @@ public class IntBoard {
 				if (j < BOARD_SIZE - 1) {
 					tempAdj.add(grid[i][j + 1]);
 				}
-				adjMtx.put(grid[i][j], tempAdj);
+				adjMtx.put(grid[i][j], tempAdj); // Add this cell as a key, and all adjacent and valid cells as the corresponding set of cells
 			}
 		}
 	}
 	
+	/** Returns the set of adjacent cells to the given cell
+	 * @param cell
+	 * @return
+	 */
 	public Set<BoardCell> getAdjList(BoardCell cell) {
 		return adjMtx.get(cell);
 	}
 	
+	/** Calculates all valid target cells the given length away from the given cell
+	 * @param startCell
+	 * @param pathLength
+	 */
 	public void calcTargets(BoardCell startCell, int pathLength) {
 		for (BoardCell c: adjMtx.get(startCell)) {
-			visited.add(startCell);
+			visited.add(startCell); // Ensure the starting cell is not revisited
 			if (visited.contains(c)) {
-				continue;
+				continue; // Do not visit the same cell twice
 			}
 			visited.add(c);
 			if (pathLength == 1) {
-				targets.add(c);
+				targets.add(c); // If at desired distance from starting cell, a target has been reached
 			} else {
-				calcTargets(c, pathLength - 1);
+				calcTargets(c, pathLength - 1); // If not at desired distance from starting cell, continue
 			}
 			visited.remove(c);
 		}
 	}
 	
+	/** Return set of targets
+	 * @return
+	 */
 	public Set<BoardCell> getTargets(){
+		// Clear target list
 		Set<BoardCell> tempTargets = new HashSet<BoardCell>(targets);
 		targets.clear();
+		
 		return tempTargets;
 	}
 	
+	/** Return the desired cell
+	 * @param row
+	 * @param column
+	 * @return
+	 */
 	public BoardCell getCell(int row, int column) {
 		return grid[row][column];
 	}
