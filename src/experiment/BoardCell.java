98ac4c3c2d680f493f2a4f771936c6e18477cd72
Sam Mills <smills1@mymail.mines.edu>
2020-02-21 17:19:30 -0700

    Finished Part 1, saved Part 1 code as a separate folder, added functionality for all adjacency tests

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -4,4 +4,16 @@ public class BoardCell {
 	private int row;
 	private int column;
 	
+	public BoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public int getCol() {
+		return this.column;
+	}
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -6,21 +6,47 @@ import java.util.*;
 
 public class IntBoard {
 	private BoardCell[][] grid;
-	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	private Map<BoardCell, HashSet<BoardCell>> adjMtx;
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
 	
+	private static final int BOARD_SIZE = 4;
+	
 	public IntBoard() {
-		grid = new BoardCell[4][4];
+		grid = new BoardCell[BOARD_SIZE][BOARD_SIZE];
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				grid[i][j] = new BoardCell(i, j);
+			}
+		}
+		adjMtx = new HashMap<BoardCell, HashSet<BoardCell>>();
 		calcAdjacencies();
 	}
 	
 	private void calcAdjacencies() {
-		
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				HashSet<BoardCell> tempAdj = new HashSet<BoardCell>();
+				for (BoardCell[] r: grid) {
+					for (BoardCell c: r) {
+						if (((c.getRow() == (i - 1) || c.getRow() == (i + 1)) && c.getCol() == j) ||
+						   ((c.getCol() == (j - 1) || c.getCol() == (j + 1)) && c.getRow() == i)) {
+							tempAdj.add(c);
+						}
+					}
+				}
+				adjMtx.put(grid[i][j], tempAdj);
+			}
+		}
 	}
 	
 	public Set<BoardCell> getAdjList(BoardCell cell) {
-		return null;
+		System.out.println(cell.getRow() + " " + cell.getCol() + ": ");
+		for (BoardCell c: adjMtx.get(cell)) {
+			System.out.print("    " + c.getRow() + " " + c.getCol() + ",  ");
+		}
+		System.out.println();
+		return adjMtx.get(cell);
 	}
 	
 	public void calcTargets(BoardCell startCell, int pathLength) {
@@ -32,7 +58,7 @@ public class IntBoard {
 	}
 	
 	public BoardCell getCell(int row, int column) {
-		return null;
+		return grid[row][column];
 	}
 	
 
