7f3185d4f2028727a86df2137acce0a1e59def32
Sam Mills <smills1@mymail.mines.edu>
2020-04-03 14:52:38 -0600

    Removed print statements

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -53,10 +53,7 @@ public class Player {
 	
 	public void dealCard(Card card) {
 		myCards.add(card);
-		System.out.println(playerName + " has " + myCards.size() + " cards.");
 		seenCards.add(card);
-		System.out.println(playerName + " has seen " + seenCards.size() + " cards.");
-		
 	}
 	
 	public void setRow(int row) {
